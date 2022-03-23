31af79ff8448b4a50869e04a45a865b32f220217
samposig <samposig@gmail.com>
2022-03-23 13:53:25 -0600

    more info

--- public/js/index.js
+++ public/js/index.js
@@ -1,35 +1,36 @@
 const customer_button = document.getElementById('customer-button');
 const customerList = documemnt.querySelector('ul');
+const employeeList = document.querySelector('ul');
 //all customers
 function getCustomers() {
     fetch('../models/customers')
         .then( response => {
            return response.json(); 
     })
- 
     .then((data) => {
         for (var i = 0; i < data.length; i++)
         var listItem = document.createElement('li');
-        listItem.textContent = data[i].html_url;
-        repoList.appendChild(listItem);
-      }
+        listItem.textContent = data[i];
+        customerList.appendChild(listItem);
     });
 }
-
-ul.appendChild(list);
-   
-};
  //all customer info, recent and past issues
 function getCustomerById() {
-
+    fetch('../')
 }
 
 //all employees
 function getEmployee(){
-    fetch('../models/employees')
-    .then (response => {
-
+    fetch('../models/employee')
+        .then( response => {
+           return response.json(); 
     })
+    .then((data) => {
+        for (var i = 0; i < data.length; i++)
+        var listItem = document.createElement('li');
+        listItem.textContent = data[i];
+        employeeList.appendChild(listItem);
+    });
 }
 
 //indivual employees

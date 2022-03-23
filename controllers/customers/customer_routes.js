a7a53eee4d6bb68e0557abac208141e1d1d35aa0
Austin A <austinandrews89@gmail.com>
2022-03-23 10:56:03 -0600

    Early versions of Customer Routes being built, ALPHA for GET ALL, GET BY ID, POST built. Update Pending

---
+++ controllers/customers/customer_routes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const Customer = require('../../models');
+
+router.get('/', async (res, req) => {
+    try {
+        const customerData = await Customer.findAll()
+        res.status(200).json(customerData);
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
+
+router.get('/:id', async (res, req) => {
+    try {
+        const soleCustomerData = await Customer.findByPk(req.params.id)
+        if (!soleCustomerData){
+            res.status(500).json({message: "No customer with that ID found"})
+            return
+        }
+        res.status(200).json(soleCustomerData);
+    } catch (err){
+        res.status(400).json(err)
+    }
+});
+
+module.exports = router;
---
+++ controllers/index.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+const customerRoutes = require('./customers')
+
+module.exports = router;

03a4cfd7f1e45d958da7964116220e1d3e6edb0b
samposig <samposig@gmail.com>
2022-03-23 13:34:42 -0600

    setup functions for fetch requests

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,52 @@
 const customer_button = document.getElementById('customer-button');
-
+const customerList = documemnt.querySelector('ul');
+//all customers
 function getCustomers() {
-    fetch('/customers')  
-        .then()
+    fetch('../models/customers')
+        .then( response => {
+           return response.json(); 
+    })
+ 
+    .then((data) => {
+        for (var i = 0; i < data.length; i++)
+        var listItem = document.createElement('li');
+        listItem.textContent = data[i].html_url;
+        repoList.appendChild(listItem);
+      }
+    });
+}
+
+ul.appendChild(list);
+   
+};
+ //all customer info, recent and past issues
+function getCustomerById() {
+
+}
+
+//all employees
+function getEmployee(){
+    fetch('../models/employees')
+    .then (response => {
+
+    })
+}
+
+//indivual employees
+function getEmployeeById() {
+
+}
+
+//
+function getOpenIssues(){
+
+}
+
+function getClosedIssues(){
+
+}
+
+
+function getUnits() {
+
 }

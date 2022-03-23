6c49321e15752dfb24fe835d112c3d9b3cc7bb98
Austin A <austinandrews89@gmail.com>
2022-03-23 13:34:12 -0600

    Trying to fix Server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');

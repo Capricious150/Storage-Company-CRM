d4ffba83cf089f1dad728921ed6face54237c62b
fonald <dkcook90@gmail.com>
2022-03-23 14:24:17 -0600

    Started to fill out seed data

--- seeds/index.js
+++ seeds/index.js
@@ -1,1 +1,6 @@
-const sequelize = require('../config/connection')
+const sequelize = require('../config/connection');
+const { Customers, Employees, Units } = require('../models');
+
+const customerSeedData = require('./customersSeedData.json');
+const employeeSeedData = require('./employeesSeedData.json');
+const unitSeedData = require('./unitsSeedData.json');

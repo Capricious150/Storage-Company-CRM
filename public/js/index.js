f977b68cba1b8223d7975939f84b671f62128464
Austin A <austinandrews89@gmail.com>
2022-03-24 16:34:05 -0600

    Updated Customers Page INCLUDES get by ID

--- public/js/index.js
+++ public/js/index.js
@@ -10,7 +10,7 @@ const getCustomers = async () => {
     })
     console.log(response);
     if (response.ok) {
-        // document.location.replace('/customer/');
+        document.location.replace('/customer/');
       } else {
         alert('Failed.');
       }

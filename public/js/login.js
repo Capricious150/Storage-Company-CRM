089a07882cc6520100613ffb96c9ae25d7f3df8e
samposig <samposig@gmail.com>
2022-03-25 13:30:21 -0600

    login info

--- public/js/login.js
+++ public/js/login.js
@@ -1,26 +1,41 @@
-const loginFormHandler = async (event) => {
-    event.preventDefault();
+// const loginFormHandler = async (event) => {
+//     event.preventDefault();
   
-    const email = document.querySelector('email').value.trim();
-    const password = document.querySelector('password').value.trim();
+//     const employee_id = document.querySelector('employee_id').value.trim();
+//     const password = document.querySelector('password').value.trim();
   
-    if (email && password) {
-      const response = await fetch('/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
+//     if (employee_id && password) {
+//       const response = await fetch('/customer', {
+//         method: 'POST',
+//         body: JSON.stringify({ employee_id, password }),
+//         headers: { 'Content-Type': 'application/json' },
+//       });
   
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to log in.');
-      }
+//       if (response.ok) {
+//         document.location.replace('/customer');
+//       } else {
+//         alert('Failed to log in.');
+//       }
+//     }
+//   };
+  
+//   document
+//     .querySelector('.container')
+//     .addEventListener('submit', loginFormHandler);
+  
+
+const loginButton = document.getElementById("btn_login");
+
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const employee_id = loginForm.employee_id.value;
+    const password = loginForm.password.value;
+
+    if (employee_id === "" && password === "true") {
+        alert("You have successfully logged in.");
+        location.replace('/customer');
+    } else {
+        loginErrorMsg.style.opacity = 1;
     }
-  };
-  
-  document
-    .querySelector('.container')
-    .addEventListener('submit', loginFormHandler);
-  
-  
+}) 
